840af1d627433caa224583b67503cc1f96984e51
sara <sara.alshayeb59@gmail.com>
2023-09-06 22:52:00 +0300

    selectionsort algorathim added

--- Main.java
+++ Main.java
@@ -20,11 +20,16 @@ public class Main {
 
     public static void main(String[] args) {
         int[] arr = {64, 25, 12, 22, 11};
-        selectionSort(arr);
+        Sort(arr);
 
+
+    }
+    public static void sort(int[] array){
+        selectionSort(array);
         System.out.println("Sorted array:");
         for (int num : arr) {
             System.out.print(num + " ");
         }
+
     }
 }
